2e93a8313bb36f5fc6ef7044cff456d943db2498
wan muzaffar Wan Hashim <wanmuz@wans-MacBook-Pro.local>
2017-08-19 12:50:55 +0800

    weather app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { SplashScreen } from '@ionic-native/splash-screen'; 
+import { HttpModule } from '@angular/http'
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	weather:any;
+
+  constructor(public navCtrl: NavController, public http:Http) {
 
   }
 
-}
+  buttonTapped(){
+  	this.http.get('http://api.openweathermap.org/data/2.5/weather?lat=3.1071334999999998&lon=101.6398306&appid=8131be7e3e6b2014b3af931e011bd730')
+  	.map(res => res.json())
+  	.subscribe(
+  		data =>	{
+  			this.weather=data
+  			console.log(data)
+  		},
+  		err => {
+  			console.log(err)
+  		},
+  		()=>{
+  			console.log('retrived')
+  		});
+  }
+
+  }
+
+
